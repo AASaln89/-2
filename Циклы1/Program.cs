74414c1b1199e9367eacd746c5ce8f0864d3f917
Andrey <kvanta13@yandex.ru>
2021-11-29 22:15:16 +0300

    Добавьте файлы проекта.

---
+++ Циклы1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace циклы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int Number;
+            int Number2 = 1;
+            int Number3 = 0;
+            for (int i = 0; i < N; i++)
+            {
+                Number = Number2 + (2 * N - 1);
+                Number3 = Number3 + 2;
+                Number2 += Number3 + 1;
+                Console.WriteLine("Квадрат равен {0}", Number);
+            }
+            {
+            }
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
